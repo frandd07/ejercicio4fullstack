a2429835f8b68ca3830111812286f3b63b7d4d41
frandd07 <150445758+frandd07@users.noreply.github.com>
2025-01-27 18:04:04 +0100

    Update completado y revisado

--- src/app/habito/create/page.js
+++ src/app/habito/create/page.js
@@ -75,6 +75,7 @@ export default function CreateHabito() {
             type="date"
             value={fecha}
             onChange={(e) => setFecha(e.target.value)}
+            min={new Date().toISOString().split("T")[0]}
             required
           />
         </label>
